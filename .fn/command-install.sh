356cb1f192087a3da4cebe09c90da0c004822039
bobmagicii <bob@pegasusgate.net>
2026-01-26 00:27:55 +0000

    refactor the install process

--- .fn/command-install.sh
+++ .fn/command-install.sh
@@ -18,19 +18,10 @@ CommandInstall() {
 
 	# grab the code.
 
-	rm -rf "${TempDir}"
-	git clone "${AcmeShRepoURL}" "${TempDir}"
-
-	# run its installer with the config.
-
-	cd "${TempDir}"
-	bash ./acme.sh $ASHCFG --install -m "${ContactEmail}"
-	bash "${ASHBIN}" $ASHCFG --set-default-ca --server "${DefaultCertAuth}"
-
-	# cleanup the install.
-
-	cd "${BaseDir}"
-	rm -rf "${TempDir}"
+	PrintH2Ln "Installing acme.sh..."
+	FetchAcmeShInstall "${AcmeShRepoURL}" "${TempDir}"
+	SetupAcmeShInstall "${TempDir}" "${ContactEmail}" "${DefaultCertAuth}"
+	CleanupAcmeShInstall "${TempDir}"
 
 	########
 
@@ -40,4 +31,56 @@ CommandInstall() {
 ################################################################################
 ################################################################################
 
-# ...
+FetchAcmeShInstall() {
+
+	local RepoURL=$1
+	local RepoDir=$2
+
+	echo ">> Downloading ${RepoURL}..."
+	rm -rf "${RepoDir}"
+	git clone "${RepoURL}" "${RepoDir}" &>/dev/null
+
+	echo "++" $(du -sh "${RepoDir}")
+	echo
+
+	return $OK
+}
+
+SetupAcmeShInstall() {
+
+	local RepoDir=$1
+	local Email=$2
+	local DefaultCert=$3
+
+	echo ">> Setting up acme.sh..."
+
+	cd "${RepoDir}"
+	bash ./acme.sh $ASHCFG --install -m "${Email}" &>/dev/null
+	bash "${ASHBIN}" $ASHCFG --set-default-ca --server "${DefaultCert}" &>/dev/null
+	cd "${BaseDir}"
+
+	echo "++" $(du -sh "${InstDir}")
+	echo
+
+	return $OK
+}
+
+CleanupAcmeShInstall() {
+
+	local RepoDir=$1
+
+	########
+
+	cd "${BaseDir}"
+
+	########
+
+	echo ">> Removing installer files..."
+	echo "--" $(du -sh "${RepoDir}")
+	rm -rf "${RepoDir}"
+	echo
+
+	########
+
+	return $OK
+}
