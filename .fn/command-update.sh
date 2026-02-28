46f760828ad1021e9b6fcc20a97ff83bb35f77d3
bobmagicii <bob@pegasusgate.net>
2026-02-28 18:11:51 +0000

    skip self update gracefully if not git cloned

--- .fn/command-update.sh
+++ .fn/command-update.sh
@@ -11,8 +11,17 @@ function CommandUpdate() {(
 	function UpdateSelf() {
 
 		local Command=""
+		local GitDir="${Config['AppRoot']}/.git"
 
 		########
+
+		if [[ ! -d $GitDir ]];
+		then
+			PrintH2Ln "Updating ashbox"
+			echo "skipping self update: not installed via git"
+			echo
+			return $KTHXBAI
+		fi
 
 		printf -v Command "git -C %s pull" "${Config['AppRoot']}"
 
